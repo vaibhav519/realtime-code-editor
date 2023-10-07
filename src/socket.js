2270700313a3f505ab2a9640afc3f3f0d2efdea2
vaibhav parmar <84400764+vaibhav519@users.noreply.github.com>
2023-10-08 01:38:05 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io("https://realtime-code-editor-black.vercel.app", options);
+    return io(process.env.REACT_APP_BACKEND_URL, options);
 };
